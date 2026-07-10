d7e242e3f69ef4263e404ec73dbde4c2b019b48a
glegigan <109355640+glegigan@users.noreply.github.com>
2026-07-10 21:25:33 +0200

    Make the window wider
    Changes resolution from 760px to 960px

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Meeting Transcriber")
-        self.setMinimumSize(760, 760)
+        self.setMinimumSize(960, 760)
 
         self.button_height = 30
         self.button_radius = 8
